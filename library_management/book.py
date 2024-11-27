825fc97ce4b1f62ba0954c20ba94410779c0bc12
Nidda Rasib <nidda_rasib@outlook.com>
2024-11-27 11:18:43 +0000

    updated Book.py

--- library_management/book.py
+++ library_management/book.py
@@ -8,11 +8,23 @@ class Book:
     isbn (str): The isbn of the book.
     """
 
+    def __init__(self, title, author, isbn):
         """
         Constructs all the necessary attributes for the book object.
 
         Parameters:
         title (str): The title of the book.
         author (str): The author of the book.
+        isbn (str): The isbn of the book.
         """
         self.title = title
+        self.author = author
+        self.isbn = isbn
+        
+
+    def __str__(self):
+        return (f"Title: {self.title}", 
+                f"Author: {self.author}", 
+                f"ISBN: {self.isbn}")
+
+        
