1480f9f08cf0826dbc39af2cd2183c2356781982
Nidda Rasib <nidda_rasib@outlook.com>
2024-12-25 15:00:17 +0000

    function added to define a borrowed or available book

--- library_management/book.py
+++ library_management/book.py
@@ -20,17 +20,21 @@ class Book:
         self.title = title
         self.author = author
         self.isbn = isbn
+        self.is_borrowed = False
         
 
     def __str__(self):
         """
-        returns a string for the book object.
+        returns a string for the book object including whether a 
+        book is available or borrowed.
 
         string contains the:
         title (str): The title of the book.
         author (str): The author of the book.
         isbn (str): The isbn of the book.
         """
+        def __str__(self):
+            status_of_book = "Borrowed" if self.is_borrowed else "Available"
         return (f"Title: {self.title}", 
                 f"Author: {self.author}", 
                 f"ISBN: {self.isbn}")
