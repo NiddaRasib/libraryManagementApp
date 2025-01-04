c712a2796e37bfb6eb9886e8e89c0ebe5d861722
Nidda Rasib <nidda_rasib@outlook.com>
2025-01-04 00:21:16 +0000

    test page updated

--- library_management/test_library.py
+++ library_management/test_library.py
@@ -36,13 +36,30 @@ Library Operations
 '''
 
 # Add books to the library
+library = Library()
+book = Book("Fluent Python", "Luciano Ramalho", "9781491946268")
+library.add_book(book)
+print("Book was added to the library")
 
 
 # Add member to the library
+library = Library()
+member = StudentMember("Ricardo Kane", "29740")
+library.add_member(member)
+print("student member was added to the library")
+
 
 # List available books in the library
+library = Library()
+book = Book("Fluent Python", "Luciano Ramalho", "9781491946268")
+library.add_book(book)
+list_available_books 
 
 # Borrow a book from the library
+library = Library()
+book = Book("Fluent Python", "Luciano Ramalho", "9781491946268")
+library.add_book(book)
+
 
 # List borrowed books from the library
 
